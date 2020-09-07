6b741dcc35789c3018af7025be19a6a32a80a917
microwerx <jmetzgar@outlook.com>
2020-09-07 14:21:20 -0800

    Add fluxions-gte-tests project

---
+++ fluxions-gte-tests/fluxions-gte-tests.cpp
@@ -0,0 +1,13 @@
+// fluxions-gte-tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <hatchetfish.hpp>
+
+#pragma comment(lib, "hatchetfish.lib")
+
+int main()
+{
+    HFLOGDEBUG("Hello, world!\n");
+    return 0;
+}
